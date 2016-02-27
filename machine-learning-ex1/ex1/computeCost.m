ab0166beb05a9ac94ed536d5032277b94126579a
Marcus Carvalho <marcuskbra@hotmail.com>
2016-02-27 11:21:28 -0300

    Implementaçao da solucao da cost function

--- machine-learning-ex1/ex1/computeCost.m
+++ machine-learning-ex1/ex1/computeCost.m
@@ -6,16 +6,21 @@ function J = computeCost(X, y, theta)
 % Initialize some useful values
 m = length(y); % number of training examples
 
+
+
 % You need to return the following variables correctly 
-J = 0;
+
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
+% h = X * theta;
+% error = h - y;
+% error_sqr = error.^2;
+% J = 1/(2*m) * sum(error_sqr);
 
-
-
+J = 1/(2*m) * sum((( X*theta)-y).^2);
 
 % =========================================================================
 
