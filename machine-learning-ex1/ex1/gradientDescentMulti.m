9ba44feaaa7616434bb5ac59af2514d1aa079d6f
Marcus Carvalho <marcuskbra@hotmail.com>
2016-02-28 18:17:55 -0300

    Implementacao do calculo de gradient descent multi variables

--- machine-learning-ex1/ex1/gradientDescentMulti.m
+++ machine-learning-ex1/ex1/gradientDescentMulti.m
@@ -16,16 +16,13 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCostMulti) and gradient here.
     %
+    h = X * theta;
+    error = h - y;
+    gradient = X'*error;
+    theta_change =  alpha * (1/m * gradient);
+    %theta_change = (X'* error) * alpha * 1/m;
 
-
-
-
-
-
-
-
-
-
+    theta = theta - theta_change;
 
     % ============================================================
 
