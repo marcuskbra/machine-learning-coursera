11689456b2c7af94ed43887b737f760fd27bc73f
Marcus Carvalho <marcuskbra@hotmail.com>
2016-03-10 20:28:08 -0300

    Implementacao de regularizacao

--- machine-learning-ex2/ex2/costFunctionReg.m
+++ machine-learning-ex2/ex2/costFunctionReg.m
@@ -7,9 +7,6 @@ function [J, grad] = costFunctionReg(theta, X, y, lambda)
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
-J = 0;
-grad = zeros(size(theta));
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta.
@@ -18,8 +15,17 @@ grad = zeros(size(theta));
 %               derivatives of the cost w.r.t. each parameter in theta
 
 
+h = sigmoid(X * theta);
+unreg_cost = 1./m * ( -y' * log(h) - ( 1 - y' ) * log ( 1 - h) );
+theta(1) = 0;
+theta_sqr = theta' * theta;
+reg_cost = theta_sqr * (lambda / (2 * m));
+J = unreg_cost + reg_cost;
 
+unreg_grad = 1./m * X' * (h - y);
+reg_grad = theta * (lambda / m);
 
+grad = unreg_grad + reg_grad;
 
 
 % =============================================================
